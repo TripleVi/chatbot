2e4102087be447b346d35a726c02ed53243750aa
Vuong Vu <vuongvu061101@gmail.com>
2024-11-06 08:41:22 +0700

    update API

--- src/app.py
+++ src/app.py
@@ -1,16 +1,17 @@
-from datetime import datetime, timezone
 import os
 from operator import itemgetter
 
 from dotenv import load_dotenv
 from flask import Flask, request
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
 from langchain_google_vertexai import ChatVertexAI, VertexAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-import mysql.connector
+from langchain.tools.retriever import create_retriever_tool
+from langchain_core.messages import HumanMessage, AIMessage
 from pinecone import Pinecone
+
+import database as db
 
 load_dotenv()
 
@@ -25,49 +26,56 @@ vector_store = PineconeVectorStore(index=index, embedding=embeddings)
 
 retriever = vector_store.as_retriever(
     search_type="similarity_score_threshold",
-    search_kwargs={"k": 20, "score_threshold": 0.8},
+    search_kwargs={"k": 20, "score_threshold": 0.7},
 )
 
-def generate_chat_title(model, question):
+def generate_chat_title(user_input: str):
     prompt_template = ChatPromptTemplate([
-        ("system", "You are a friendly assistant. Provide a concise Vietnamese title to the conversation based on the following question. Please respond only with the title in affirmative form, without any special characters (e.g., '.', '?') at the end."),
-        ("user", "{text}")
+        ("system", "You are a friendly assistant. Provide a concise Vietnamese title to the conversation based on the following question. Please respond only with the title in affirmative form, without any special characters (e.g., '.', '?', '\n') at the end."),
+        ("human", "{text}")
     ])
     chain = prompt_template | model | StrOutputParser()
-    response = chain.invoke({"text": question})
+    response = chain.invoke({"text": user_input})
     return response.strip()
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+def get_chat_history(id: int):
+    raw_messages = db.get_messages(id)
+    return [
+        HumanMessage(m["content"]) if i % 2 == 0 else AIMessage(m["content"])
+        for i, m in enumerate(raw_messages)
+    ]
 
-def chatbot(user_input):
-    system_template = """
-    The system is designed to store and manage Greenwich University students' graduation projects. You are a friendly assistant tasked with answering questions about these projects in Vietnamese. Use only the provided context below to answer, keeping responses concise and, if possible, in bullet points. If the information isn't available in the context, respond with "I don't know". Avoid generating answers without relevant context.
-    Context: {context}
-    """
+def get_tools():
+    retrieve_projects = create_retriever_tool(
+        retriever,
+        "retrieve_graduation_projects",
+        "Searches and returns excerpts from Vietnamese articles about University of Greenwich students' graduation projects.",
+    )
+    available_tools = {"retrieve_graduation_projects": retrieve_projects}
+    return (available_tools, [retrieve_projects])
+
+def chatbot(*, chat_id: int, user_input: str):
     prompt_template = ChatPromptTemplate([
-        ("system", system_template),
-        ("user", "{text}")
+        ("system", "The system is designed to store and manage Greenwich University students' graduation projects. You are a friendly assistant tasked with answering questions about these projects in Vietnamese. Use only the provided context to answer, keeping responses concise and, if possible, in bullet points. If the information isn't available in the context, just say that you don't know. Avoid generating answers without relevant context."),
+        MessagesPlaceholder("chat_history")
     ])
-    chain = (
-        {"context": retriever | format_docs, "text": RunnablePassthrough()}
-        | prompt_template
-        | model
-        | StrOutputParser()
-    )
-    response = chain.invoke(user_input)
-    return response.strip()
+    available_tools, tools = get_tools()
+    model_with_tools = model.bind_tools(tools)
+    chain = prompt_template | model_with_tools
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host=os.environ["DB_HOST"],
-        port=os.environ["DB_PORT"],
-        user=os.environ["DB_USERNAME"],
-        password=os.environ["DB_PASSWORD"],
-        database=os.environ["DB_DATABASE"]
-    )
-
-sample_response = "Ứng dụng di động theo dõi sức khỏe tim mạch là một giải pháp công nghệ hỗ trợ người dùng kiểm tra và giám sát tình trạng tim mạch của mình một cách hiệu quả và tiện lợi. Ứng dụng cung cấp các tính năng ghi lại các chỉ số như nhịp tim, huyết áp, và mức độ hoạt động thể chất, giúp người dùng có thể theo dõi sự biến đổi của các chỉ số này theo thời gian. Ngoài ra, ứng dụng còn cung cấp các lời khuyên sức khỏe cá nhân hoá dựa trên các chỉ số đã thu thập, giúp người dùng có những hướng dẫn kịp thời và phù hợp để duy trì sức khỏe tim mạch. Mục tiêu của dự án là tạo ra một công cụ hữu ích, dễ sử dụng để người dùng có thể nâng cao nhận thức và cải thiện sức khỏe tim mạch của mình.\n\n- Ứng dụng di động theo dõi sức khỏe tim mạch là một giải pháp công nghệ hỗ trợ người dùng kiểm tra và giám sát tình trạng tim mạch của mình một cách hiệu quả và tiện lợi.\n- Ứng dụng di động theo dõi sức khỏe tim mạch là một giải pháp công nghệ hỗ trợ người dùng kiểm tra và giám sát tình trạng tim mạch của mình một cách hiệu quả và tiện lợi.\n- Ứng dụng di động theo dõi sức khỏe tim mạch là một giải pháp công nghệ hỗ trợ người dùng kiểm tra và giám sát tình trạng tim mạch của mình một cách hiệu quả và tiện lợi.\n- Ứng dụng di động theo dõi sức khỏe tim mạch là một giải pháp công nghệ hỗ trợ người dùng kiểm tra và giám sát tình trạng tim mạch của mình một cách hiệu quả và tiện lợi.\n- Ứng dụng di động theo dõi sức khỏe tim mạch là một giải pháp công nghệ hỗ trợ người dùng kiểm tra và giám sát tình trạng tim mạch của mình một cách hiệu quả và tiện lợi."
+    chat_history = get_chat_history(chat_id)
+    chat_history.append(HumanMessage(user_input))
+    global ai_msg
+    ai_msg = chain.invoke({"chat_history": chat_history})
+    while ai_msg.tool_calls:
+        chat_history.append(ai_msg)
+        for tool_call in ai_msg.tool_calls:
+            selected_tool = available_tools[tool_call["name"].lower()]
+            tool_msg = selected_tool.invoke(tool_call)
+            chat_history.append(tool_msg)
+        ai_msg = chain.invoke({"chat_history": chat_history})
+    
+    return ai_msg.content.strip()
 
 @app.route("/chats", methods=["POST"])
 def handle_new_chat():
@@ -75,75 +83,41 @@ def handle_new_chat():
         content, user_id = (
             itemgetter("content", "userId")(request.get_json())
         )
-        # title = generate_chat_title(model, content)
-        title = "chat title"
-        cnx = get_db_connection()
-        cur = cnx.cursor(dictionary=True)
-        add_chat = """
-            INSERT INTO chat (user_id, title, created_at)
-            VALUES (%s, %s, %s)
-        """
-        now = datetime.now(timezone.utc)
-        data_chat = (user_id, title, now)
-        cur.execute(add_chat, data_chat)
-        chat_id = cur.lastrowid
-        add_message = """
-            INSERT INTO message (content, sender, chat_id, created_at)
-            VALUES (%s, %s, %s, %s)
-        """
-        data_message = (content, "user", chat_id, now)
-        cur.execute(add_message, data_message)
-        
-        # response = chatbot(content)
-        response = sample_response
-        now = datetime.now(timezone.utc)
-        data_message = (response, "assistant", chat_id, now)
-        cur.execute(add_message, data_message)
-        message_id = cur.lastrowid
-        cnx.commit()
+        title = generate_chat_title(content)
+        chat_id = db.add_chat(user_id=user_id, title=title)
+        response = chatbot(chat_id=chat_id, user_input=content)
+        db.add_message(content=content, sender="user", chat_id=chat_id)
+        message_id = db.add_message(content=response, sender="assistant", chat_id=chat_id)
         return {
             "chat_id": chat_id,
-            "message_id": message_id,
+            "message_id": message_id
         }, 201
-    except mysql.connector.Error as err:
-        return {'error': str(err)}, 500
-    finally:
-        cur.close()
-        cnx.close()
+    except Exception as err:
+        print(err)
+        return 'Internal Server', 500
 
 @app.route("/chats/<chat_id>/messages", methods=["POST"])
 def handle_chat(chat_id):
     try:
         content = itemgetter("content")(request.get_json())
-        cnx = get_db_connection()
-        cur = cnx.cursor(dictionary=True)
-        get_chat = "SELECT * FROM chat WHERE id = %s"
-        cur.execute(get_chat, (chat_id,))
-        chat = cur.fetchone()
+        chat = db.get_chat(chat_id)
         if not chat:
             return "Not Found", 404
-        add_message = """
-            INSERT INTO message (content, sender, chat_id, created_at)
-            VALUES (%s, %s, %s, %s)
-        """
-        now = datetime.now(timezone.utc)
-        data_message = (content, "user", chat_id, now)
-        cur.execute(add_message, data_message)
-
-        response = sample_response
-        now = datetime.now(timezone.utc)
-        data_message = (response, "assistant", chat_id, now)
-        cur.execute(add_message, data_message)
-        message_id = cur.lastrowid
-
-        cnx.commit()
+        response = chatbot(chat_id=chat_id, user_input=content)
+        db.add_message(content=content, sender="user", chat_id=chat_id)
+        message_id = db.add_message(content=response, sender="assistant", chat_id=chat_id)
         return {"message_id": message_id}, 201
-    except mysql.connector.Error as err:
-        return {'error': str(err)}, 500
-    finally:
-        cur.close()
-        cnx.close()
+    except Exception as err:
+        print(err)
+        return 'Internal Server', 500
+    
+@app.route("/projects/<project_id>", methods=["POST"])
+def on_project_added(project_id):
+    status = itemgetter("status")(request.get_json())
+    print(status)
+    print(project_id)
+    return "No Content", 204
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(port=port)
+    app.run(port=port, debug=True)
---
+++ src/database.py
@@ -0,0 +1,84 @@
+import os
+from datetime import datetime, timezone
+
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        user=os.environ["DB_USERNAME"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_DATABASE"]
+    )
+
+def get_chat(id: int):
+    try:
+        cnx = get_db_connection()
+        cur = cnx.cursor(dictionary=True)
+        get_chat = "SELECT * FROM chat WHERE id = %s"
+        cur.execute(get_chat, (id,))
+        chat = cur.fetchone()
+        return chat
+    finally:
+        cur.close()
+        cnx.close()
+
+def get_messages(chat_id: int):
+    try:
+        cnx = get_db_connection()
+        cur = cnx.cursor(dictionary=True)
+        get_messages = "SELECT * FROM message WHERE chat_id = %s"
+        cur.execute(get_messages, (chat_id,))
+        messages = cur.fetchall()
+        return messages
+    finally:
+        cur.close()
+        cnx.close()
+
+def get_message(id: int):
+    try:
+        cnx = get_db_connection()
+        cur = cnx.cursor(dictionary=True)
+        get_message = "SELECT * FROM message WHERE id = %s"
+        cur.execute(get_message, (id,))
+        message = cur.fetchone()
+        return message
+    finally:
+        cur.close()
+        cnx.close()
+
+def add_message(*, content, sender, chat_id) -> int:
+    try:
+        cnx = get_db_connection()
+        cur = cnx.cursor(dictionary=True)
+
+        add_message = """
+            INSERT INTO message (content, sender, chat_id, created_at)
+            VALUES (%s, %s, %s, %s)
+        """
+        now = datetime.now(timezone.utc)
+        data_message = (content, sender, chat_id, now)
+        cur.execute(add_message, data_message)
+        cnx.commit()
+        return cur.lastrowid
+    finally:
+        cur.close()
+        cnx.close()
+
+def add_chat(*, user_id, title) -> int:
+    try:
+        cnx = get_db_connection()
+        cur = cnx.cursor(dictionary=True)
+        add_chat = """
+            INSERT INTO chat (user_id, title, created_at)
+            VALUES (%s, %s, %s)
+        """
+        now = datetime.now(timezone.utc)
+        data_chat = (user_id, title, now)
+        cur.execute(add_chat, data_chat)
+        cnx.commit()
+        return cur.lastrowid
+    finally:
+        cur.close()
+        cnx.close()
